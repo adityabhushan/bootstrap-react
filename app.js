9886c6d0a7c217e9a073d97f2411553006160e31
Aditya <aditya@rangde.org>
2017-04-03 15:19:20 +0530

    Complete - 004 Component Props

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').S  erver(app);
+const http = require('http').Server(app);
 const path = require('path')
 
 app.set('port', (process.env.PORT || 3000))
